fa95409ae385208daec5481637da44863d32c48d
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-12 18:08:30 +0530

    Defining test case

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=60*_4vr0qhh2esq-0abfgs+5o6ux4=2#wx*_*zkf)zhoix-%h')
+# , 'django-insecure-=60*_4vr0qhh2esq-0abfgs+5o6ux4=2#wx*_*zkf)zhoix-%h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 is true
