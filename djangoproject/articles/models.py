a34e598cb4b92b5b3f9df6b28ba24e163d01ebd9
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-12 21:51:50 +0530

    Modifying models

--- djangoproject/articles/models.py
+++ djangoproject/articles/models.py
@@ -1,7 +1,13 @@
 from pyexpat import model
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Articles(models.Model): # All the class in models must import from models.Model
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # auto_now : Whenever Model saved, time added
+    # auto_now_add : Whenever model added, time added
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
