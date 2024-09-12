8ffc213fac98bc92918236ccb647972a39d99b38
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-12 11:36:34 +0530

    .env setup

--- djangoproject/accounts/views.py
+++ djangoproject/accounts/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def login_view(request):
+    # Method - 1
+    # if request.method == "POST":
+    #     username = request.POST.get("username")
+    #     password = request.POST.get("password")
+    #     # print(username, password)
+    #     # User authentication
+    #     user = authenticate(request, username=username, password=password)
+    #     if user is None:
+    #         context = {"error": "Invalid username or password"}
+    #         return render(request, "accounts/login.html", context=context)
+    #     login(request, user)
+    #     return redirect('/')
+    # return render(request, "accounts/login.html", {})
+
+    # Method - 2 : using authentication form
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        # print(username, password)
-        # User authentication
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "accounts/login.html", context=context)
-        login(request, user)
-        return redirect('/')
-    return render(request, "accounts/login.html", {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {"form": form}    
+    return render(request, "accounts/login.html", context)
 
 def logout_view(request):
     if request.method=="POST":
--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=60*_4vr0qhh2esq-0abfgs+5o6ux4=2#wx*_*zkf)zhoix-%h'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=60*_4vr0qhh2esq-0abfgs+5o6ux4=2#wx*_*zkf)zhoix-%h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 is true
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
