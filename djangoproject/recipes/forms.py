2de972b46e0d2a1414966c6f697e03e06aeed39a
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-15 09:38:03 +0530

    Css rendering

--- djangoproject/recipes/forms.py
+++ djangoproject/recipes/forms.py
@@ -1,10 +1,28 @@
+from dataclasses import fields
+from logging import PlaceHolder
 from django import forms
 from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = "error-field"
+    required_css_class = "required-field"
+    # name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Recipe name"}))
+    # description = forms.CharField(widget=forms.Textarea(attrs={"rows":3}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions'] 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            new_data = {
+                "placeholder":f"Recipe {field}",
+                "class": "form-control"
+            }
+            self.fields[str(field)].widget.attrs.update(new_data)
+
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class':"form-control-2"})
 
 class RecipeIngredientForm(forms.ModelForm):
     class Meta:
