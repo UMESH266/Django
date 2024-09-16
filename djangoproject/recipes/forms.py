d3b9bf7bcbe53c3c35ea13883f7ebde375ce5c20
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-16 13:14:35 +0530

    Javascript form rendering

--- djangoproject/recipes/forms.py
+++ djangoproject/recipes/forms.py
@@ -6,6 +6,10 @@ from .models import Recipe, RecipeIngredient
 class RecipeForm(forms.ModelForm):
     error_css_class = "error-field"
     required_css_class = "required-field"
+    # Help text to name widget
+    name = forms.CharField(help_text='This is your help! <a href="/contact">Contact us</a>')
+
+    # Refere documentation for built in widgets
     # name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Recipe name"}))
     # description = forms.CharField(widget=forms.Textarea(attrs={"rows":3}))
     class Meta:
